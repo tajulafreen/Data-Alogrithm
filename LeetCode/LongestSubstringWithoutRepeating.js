b41fa4d9b639263a51d671c0dc887b176aba7fcf
afreen shaik <affushaik9493@gmail.com>
2023-12-02 04:20:59 +0530

    used for loop and while loop

---
+++ LeetCode/LongestSubstringWithoutRepeating.js
@@ -0,0 +1,36 @@
+var lengthOfLongestSubstring = function (s) {
+    let set = new Set();
+    let left = 0;
+    let maxSize = 0;
+
+    if (s.length === 0) return 0;
+    if (s.length === 1) return 1;
+
+    for (let i = 0; i < s.length; i++) {
+
+        while (set.has(s[i])) {
+            set.delete(s[left])
+            left++;
+        }
+        set.add(s[i]);
+        maxSize = Math.max(maxSize, i - left + 1)
+    }
+    return maxSize;
+}
+
+// Example 1:
+
+// Input: s = "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3.
+// Example 2:
+
+// Input: s = "bbbbb"
+// Output: 1
+// Explanation: The answer is "b", with the length of 1.
+// Example 3:
+
+// Input: s = "pwwkew"
+// Output: 3
+// Explanation: The answer is "wke", with the length of 3.
+// Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.

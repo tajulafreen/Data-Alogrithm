bd7401012ad4294d4561db89b1a18294ccc20206
afreen shaik <affushaik9493@gmail.com>
2023-12-03 02:52:32 +0530

    add testing in solution python

---
+++ HackerRank/Day27.py
@@ -0,0 +1,19 @@
+// Testing:= in python 3
+class TestDataEmptyArray:
+def get_array():
+    return []
+    
+class TestDataUniqueValues():
+def get_array():
+    return [32, 53, 825, 532]
+    
+def get_expected_result():
+    return 0
+    
+class TestDataExactlyTwoDifferentMinimums:
+def get_array():
+    return [34598, 345, 345, 3456, 643]
+
+def get_expected_result():
+    return 1
+

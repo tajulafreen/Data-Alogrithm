a0c0ec8559917a02fd50964f3288665fca115dca
afreen shaik <affushaik9493@gmail.com>
2024-01-07 14:50:28 +0530

    add solution for buy and sell clock 1 & 2

---
+++ LeetCode/BuySellClock1.js
@@ -0,0 +1,33 @@
+var maxProfit = function (prices) {
+  let left = 0;
+  let right = 1;
+  let maxProfit = 0;
+
+  while (right < prices.length) {
+    if (prices[left] < prices[right]) {
+      let profit = prices[right] - prices[left];
+      maxProfit = Math.max(maxProfit, profit);
+    } else {
+      left = right;
+    }
+    right++;
+  }
+  return maxProfit;
+};
+
+// Example 1:
+
+// Input: prices = [7,1,5,3,6,4]
+// Output: 5
+// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+// Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+// Example 2:
+
+// Input: prices = [7,6,4,3,1]
+// Output: 0
+// Explanation: In this case, no transactions are done and the max profit = 0.
+
+// Constraints:
+
+// 1 <= prices.length <= 105
+// 0 <= prices[i] <= 104
